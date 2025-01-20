776e0c34c92136b5e97e3fdf5a4fa82fcf67d49d
Nathaniel Chavez <nathanielchavez.codes@gmail.com>
2025-01-20 13:04:05 +0800

    add spring boot app scaffold

---
+++ internet-speed-checker/src/main/java/dev/wonderingfire/speedchecker/internet_speed_checker/InternetSpeedCheckerApplication.java
@@ -0,0 +1,13 @@
+package dev.wonderingfire.speedchecker.internet_speed_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetSpeedCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetSpeedCheckerApplication.class, args);
+	}
+
+}
---
+++ internet-speed-checker/src/test/java/dev/wonderingfire/speedchecker/internet_speed_checker/InternetSpeedCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wonderingfire.speedchecker.internet_speed_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetSpeedCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
